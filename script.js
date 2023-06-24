0942325b9724b0c009f4e8ac30a06ec2328ebb95
PRIYANKA.M <101044418+PRIYANKAM05@users.noreply.github.com>
2023-06-24 14:15:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function count(){
-    let words=document.getElementById("input").value; 
-    let arr=words.split(" ")
-    let ans=arr.length
-    let result = document.getElementById("result")
-    result.innerHTML = ans + "Words"
-}
+function count() {
+    let words = document.getElementById("input").value;
+    let arr = words.split(" ");
+    let ans = arr.length;
+    let result = document.getElementById("result");
+  
+    if (words.trim() === "") {
+      result.innerHTML = "Result: 0 Words"; // Display 0 when there are no words
+    } else {
+      result.innerHTML = "Result: " + ans + " Words";
+    }
+  }
+  
+  // Call count() function to initialize the result
+  count();
