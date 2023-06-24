212d07bc2f75d53d0f9afa1fa0c0e1111ebcba20
PRIYANKA.M <101044418+PRIYANKAM05@users.noreply.github.com>
2023-06-24 14:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function count() {
-    let words = document.getElementById("input").value;
-    let arr = words.split(" ");
-    let ans = arr.length;
-    let result = document.getElementById("result");
+  let words = document.getElementById("input").value;
+  let arr = words.split(" ");
+  let filteredArr = arr.filter(word => word !== ""); // Filter out empty words
+  let ans = filteredArr.length;
+  let result = document.getElementById("result");
+  result.innerHTML = "Result: " + ans + " Words";
   
-    if (words.trim() === "") {
-      result.innerHTML = "Result: 0 Words"; // Display 0 when there are no words
-    } else {
-      result.innerHTML = "Result: " + ans + " Words";
-    }
-  }
   
+}
   // Call count() function to initialize the result
   count();
